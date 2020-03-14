c8f2dc5e543b5a7632740592a70cde0fcaf75440
tringuyen <tri97nguyen@gmail.com>
2020-03-14 16:06:09 -0400

    functional test now has DRY to test todo task

--- functional_test.py
+++ functional_test.py
@@ -9,6 +9,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
+    def check_task_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
     def test_visitor_visit_page(self):
         #user go to the website
         self.browser.get('http://localhost:8000')
@@ -21,38 +25,23 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 
-        # she enter the todo task
-        inputbox.send_keys('Buy peacock feathers')
-        # when she hits enter, the page stores the task and display it back
+        inputbox.send_keys('1: Buy peacock feathers')
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == 'Buy peacock feathers' for row in rows),
-            f"the todo task is not in the row. Content was \n{table.text} for table.text ",
-        )
-
-        # after see the first todo task, she put in the second task and hit enter
-        time.sleep(1)
+        self.check_task_in_table('1: Buy peacock feathers')
+        # There is still a text box inviting her to add another item. She
+        # ​# enters "Use peacock feathers to make a fly" (Edith is very
+        # ​# methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('2: Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        self.assertIn('2: Use peacock feathers to make a fly',table.text)
-        
+        time.sleep(1)
+        self.check_task_in_table('1: Buy peacock feathers')
+        self.check_task_in_table('2: Use peacock feathers to make a fly')
 
-
-        # there is still a text box inviting her to add another item.
-        # she enters "use peacock feathers to make a fly"
-        
         self.fail('finish the test')
-
-        # the page update again and shows both items on the list
         
-    
-        
-        
-
 if __name__ == '__main__':
     unittest.main()
