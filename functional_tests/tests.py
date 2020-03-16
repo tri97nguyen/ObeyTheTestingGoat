b72e3c9a8eb84dc8338b7d99d0c6998e2c8eb125
tringuyen <tri97nguyen@gmail.com>
2020-03-16 18:20:12 -0400

    new funtional test for multiple user,

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 import unittest
 import time
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -16,6 +18,21 @@ class NewVisitorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, (row.text for row in rows))
+                return  
+            except (AssertionError, WebDriverException) as e:
+                if time.time()- start_time > MAX_WAIT:
+                    raise e
+                # time.sleep(0.5)
+
+
 
     def test_visitor_visit_page(self):
         #user go to the website
@@ -34,7 +51,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item. She
         # ​# enters "Use peacock feathers to make a fly" (Edith is very
@@ -42,11 +59,58 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_task_in_table('1: Buy peacock feathers')
-        self.check_task_in_table('2: Use peacock feathers to make a fly')
+        
+        
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('finish the test')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith opens browswer and go to her dedicated website
+        self.browser.get(self.live_server_url)
+        # Edith sees her unique URL
+        self.assertRegex(self.browser.current_url,'/list/.+')
+        edith_url = self.browser.current_url
+        # Edith start endtering her first todo
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        # Edith start entering her second todo
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table("Use peacock feathers to make a fly")
+        # After seeing the todos, she close the browser and goes to bed
+        self.browser.quit()
+
+        # Now a new user, Francis, visit the website
+        # Francis open browser and enter site url
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        # Francis sees his unique url        
+        self.assertRegex(self.browser.current_url,'/list/.+')
+        # Francis url is unique form Edith
+        francis_url = self.browser.current_url
+        self.assertNotEqual(edith_url, francis_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+
+        # The website must not contain Edith saved todo list
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('Use peacock feathers to make a fly',page_text)
+
+        # After seeing his page is blank, Francis start entering todo tasks
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis hits enter and sees todo tasks appear
+        self.wait_for_row_in_list_table('buy milk')
+        
+
+
         
 # if __name__ == '__main__':
 #     unittest.main()
