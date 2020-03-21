83394a6c975530973b81c0f00710ec5d456778c1
tringuyen <tri97nguyen@gmail.com>
2020-03-20 23:25:46 -0400

    functional test first step to test layout+styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -116,6 +116,29 @@ class NewVisitorTest(LiveServerTestCase):
         francis_url = self.browser.current_url
         self.assertNotEqual(edith_url, francis_url)
 
+    def test_layout_and_styling(self):
+        #Edith goes to homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notices the input is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
+
+        # she enter a new todo task and notices the inputbox also at the center
+        inputbox.send_keys('a new todo list item')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: a new todo list item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
         
         
         
