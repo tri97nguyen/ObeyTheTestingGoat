a00e1a049c81f0153e63e30257c8900139d827e5
tringuyen <tri97nguyen@gmail.com>
2020-03-22 18:04:03 -0400

    hack ALLOWEDC_HOST to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b4qoz7_lo3+)&%+754g94mo2ubo6up#kj_v%+01n)d5-rqhnoo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
