830aed714f6338f8cb936d9d0a7efeebbea926e5
tringuyen <tri97nguyen@gmail.com>
2020-03-29 01:18:19 -0400

    split list unit test into two files

--- list/tests/test_views.py
+++ list/tests/test_views.py
@@ -8,32 +8,6 @@ from list.models import Item,List
 from selenium import webdriver
 # Create your tests here.
 # this is second line
-class ListAndItemModelTest(TestCase):
-
-    def test_save_and_retrieve_item_and_list(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'the first ever list item'
-        first_item.listy = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'the second item'
-        second_item.listy = list_
-        second_item.save()
-
-        saved_list = List.objects.all()
-        
-        self.assertIn(list_,saved_list)
-
-        self.assertEqual(first_item, Item.objects.all().first())
-        self.assertEqual(Item.objects.all()[0].listy, list_)
-        
-        self.assertEqual(second_item, Item.objects.all()[1])
-        self.assertEqual(second_item.listy, list_)
-        
 
 
 
